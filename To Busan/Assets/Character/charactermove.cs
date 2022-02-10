554ed373426f93f0821cc9c222c2bc03329fff39
jisun0415 <88261608+jisun0415@users.noreply.github.com>
2022-02-10 17:44:09 +0900

    캐릭터마우스+키보드 이동

--- To Busan/Assets/Character/charactermove.cs
+++ To Busan/Assets/Character/charactermove.cs
@@ -9,18 +9,23 @@ public class charactermove : MonoBehaviour
     public float speed = 10000000f;
     public bool isMoveable = true;
     public Animator animator;
+    //public int  PlayerSetting=0;
+
     void Start()
     {
+        animator = GetComponent<Animator>();
+        // if (hasAuthority)
+        // {
         Camera cam = Camera.main;
         cam.transform.SetParent(transform);
         cam.transform.localPosition = new Vector3(0f, 0f, -1f);
         cam.orthographicSize = 5;
-
+        // }
 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Move();
     }
@@ -28,24 +33,38 @@ public class charactermove : MonoBehaviour
 
     public void Move()
     {
+
         if (isMoveable)
         {
-            /*
-            if(PlayerSettings.controlType == EControlType.KeyboardMouse)
+            bool isMove = false;
+
             {
+                Vector3 dir = Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f), 5f);
+                if (dir.x < 0f) transform.localScale = new Vector3(-1f, 1f, 1f);
+                else if (dir.x > 0f) transform.localScale = new Vector3(1f, 1f, 1f);
+                transform.position += dir * speed * Time.deltaTime;
+                isMove = dir.magnitude != 0f;
 
             }
-            */
-            bool isMove = false;
-            Vector3 dir = Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f), 5f);
-            if (dir.x < 0f) transform.localScale = new Vector3(-1f, 1f, 1f);
-            else if (dir.x > 0f) transform.localScale = new Vector3(1f, 1f, 1f);
 
-            transform.position += dir * speed * Time.deltaTime;
-            isMove = dir.magnitude != 0f;
-            Debug.Log(isMove);
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    Vector3 dir = (Input.mousePosition - new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f)).normalized;
+
+                    if (dir.x < 0f) transform.localScale = new Vector3(-1f, 1f, 1f);
+                    else if (dir.x > 0f) transform.localScale = new Vector3(1f, 1f, 1f);
+                    transform.position += dir * speed * Time.deltaTime;
+                    isMove = dir.magnitude != 0f;
+
+                }
+            }
             animator.SetBool("isWalking", isMove);
-        }
-    }
 
+
+
+        }
+
+
+    }
 }
