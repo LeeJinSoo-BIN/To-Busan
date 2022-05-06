7d17746ce5f60eb7882cadd9187c33214d225b71
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-05-06 23:53:00 +0900

    스크롤 줌인줌아웃
    .

--- To Busan/Assets/game/script/Characterssss.cs
+++ To Busan/Assets/game/script/Characterssss.cs
@@ -4,31 +4,73 @@ using UnityEngine;
 
 public class Characterssss : MonoBehaviour
 {
+   /* public Camera Cam1;
+    public Camera Cam2;
+    public Camera Cam3;
+ 
     private bool state;
-    public GameObject Target;
-    // Start is called before the first frame update
+
+
     void Start()
     {
         state = true;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if(Input.GetMouseButtonDown(1))
+       if (Input.GetMouseButtonDown(1))
         {
-            if(state==true)
+            if (state == true)
             {
-                Target.SetActive(false);
+               
+               light1.intensity += 1;
+              // light2.OuterRadiusSize += 1;
+                Cam1.orthographicSize += 1;
+                Cam2.orthographicSize += 1;
+                Cam3.orthographicSize += 1;
                 state = false;
             }
             else
             {
-                Target.SetActive(true);
+                //light1.InnerRadiusSize -= 1;
+               // light2.OuterRadiusSize -= 1;
+                Cam1.orthographicSize -= 1;
+                Cam2.orthographicSize -= 1;
+                Cam3.orthographicSize -= 1;
                 state = true;
             }
-        }
 
-       
+        }
+        
+     }
+   */
+
+         private bool state;
+         public GameObject Target;
+
+         void Start()
+         {
+             state = true;
+         }
+
+
+         void Update()
+         {
+             if(Input.GetMouseButtonDown(1))
+             {
+                 if(state==true)
+                 {
+                     Target.SetActive(false);
+                     state = false;
+                 }
+                 else
+                 {
+                     Target.SetActive(true);
+                     state = true;
+                 }
+             }
+
+
+         }
     }
-}
